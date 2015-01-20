d5334c6df2465c9c21ddd17a3c6dbf0f3e229496
David <yoxisem544@gmail.com>
2015-01-20 19:46:25 +0800

    hi

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var http2 = require('http').Server(app);
 var io = require('socket.io')(http);
-var io2 = require('socket.io')(http2);
+// var http2 = require('http').Server(app);
+// var io2 = require('socket.io')(http2);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var allClients = [];
+var people = {};
+
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-  	console.log(msg);
-    io.emit('chat message', msg);
-  });
+	allClients.push(socket);
+	// console.log(allClients);
+	console.log("client = " + allClients.length);
+	socket.on('chat message', function(msg){
+		console.log(msg);
+		io.emit('chat message', msg);
+	});
+
+	socket.on('join', function(name) {
+		people[socket.id] = name;
+		io.emit("join", name);
+		console.log(name);
+		console.log(people);
+	})
+
+	socket.on('disconnect', function() {
+		console.log('someone disconnected from server');
+
+		var i = allClients.indexOf(socket);
+		io.emit('chat message', people[socket.id]+" has leaved.")
+		console.log('client ' + i + ' disconnected from server');
+		delete allClients[i];
+		delete people[socket.id];
+		// clean empty clients
+		allClients = allClients.filter(function(n){ return n !== undefined; });
+	})
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-http2.listen(3001, function(){
-  console.log('listening on *:3001');
-});
+// http2.listen(3001, function(){
+//   console.log('listening on *:3001');
+// });
 
-io2.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-  	console.log(msg);
-    io2.emit('chat message', msg);
-  });
-});
+// io2.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//   	console.log(msg);
+//     io2.emit('chat message', msg);
+//   });
+// });
